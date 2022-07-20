d5ede42207ea982ac2b8bd67080f62778c056d41
ibrat-muslim <imuslimbekov1421@gmail.com>
2022-07-21 02:06:04 +0500

    One Line Deleted

--- FinishedEventArgs.cs
+++ FinishedEventArgs.cs
@@ -3,5 +3,4 @@ public class FinishedEventArgs : EventArgs
     public int SurahNumber { get; internal set; }
     public long EllapsedTime {get; internal set; }
     public string? FileFullPath { get; internal set; }
-
 }
